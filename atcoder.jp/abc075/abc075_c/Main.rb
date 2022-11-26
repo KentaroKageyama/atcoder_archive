f77de7acbde39fe26b5a0bf09d180742c6830ed1
Taroon29 <f97.kageyama.kentaro@gmail.com>
2022-11-26 11:00:22 +0000

    [AC] abc075 abc075_c

---
+++ atcoder.jp/abc075/abc075_c/Main.rb
@@ -0,0 +1,49 @@
+class UnionFind
+  def initialize(size)
+    @rank = Array.new(size, 0)
+    @parent = Array.new(size, &:itself)
+  end
+
+  def unite(id_x, id_y)
+    x_parent = get_parent(id_x)
+    y_parent = get_parent(id_y)
+    return if x_parent == y_parent
+
+    if @rank[x_parent] > @rank[y_parent]
+      @parent[y_parent] = x_parent
+    else
+      @parent[x_parent] = y_parent
+      @rank[y_parent] += 1 if @rank[x_parent] == @rank[y_parent]
+    end
+  end
+
+  def get_parent(id_x)
+    @parent[id_x] == id_x ? id_x : (@parent[id_x] = get_parent(@parent[id_x]))
+  end
+
+  def same_parent?(id_x, id_y)
+    get_parent(id_x) == get_parent(id_y)
+  end
+end
+
+N, M = gets.split.map(&:to_i)
+union = UnionFind.new(N)
+side = []
+M.times do |i|
+    a, b = gets.split.map(&:to_i)
+    side << [a - 1, b - 1]
+end
+ans = 0
+M.times do |i|
+    union = UnionFind.new(N)
+    M.times do |j|
+        union.unite(side[j][0], side[j][1]) if i != j
+    end
+    
+    count = 0
+    N.times do |j|
+        count += 1 if union.get_parent(j) == j
+    end
+    ans += 1 if count != 1
+end
+puts ans
